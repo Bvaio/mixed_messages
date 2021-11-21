9dd66c266192e480f1b1f176eaea1ee89eb687b1
xLee <thmasleemail@gmail.com>
2021-11-20 22:42:59 -0500

    moved object into function, and added more var and func

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,14 +1,20 @@
-const Messages = {
-  subjectWord: ['Lone Wolf', 'Cabbage', 'Big Spider', 'Solid Cheese', 'Rhino', 'Aviation', 'Expert', 'Profit', 'Esteemed', ''],
-  centerWord: ['to', 'from', 'the', 'glass', 'titled', 'named', 'fixing', 'mastering', 'finds', 'finding', 'looking', 'vs', '', 'tum tum', 'loop'],
+const generateMesssage = () => {
+  const Messages = {
+    subjectWord: ['Lone Wolf', 'Cabbage', 'Big Spider', 'Solid Cheese', 'Rhino', 'Aviation', 'Expert', 'Profit', 'Esteemed', ''],
+    centerWord: ['to', 'from', 'the', 'glass', 'titled', 'named', 'fixing', 'mastering', 'finds', 'finding', 'looking', 'vs', '', 'tum tum', 'loop'],
+    finalWord: ['to the edge!', 'clammed!', 'the flavored meal.', 'watered since Thursday', 'open to negotiations', 'I don\'t know why this is here.', 'out of breath.'],
+  };
+
+  const mess = Messages;
+
+  const randomGenerator = (object) => object[Math.floor(Math.random() * object.length)];
+
+  const randomOne = randomGenerator(mess.subjectWord);
+  const randomTwo = randomGenerator(mess.centerWord);
+  const randomThree = randomGenerator(mess.subjectWord).toLowerCase();
+  const randomFour = randomGenerator(mess.finalWord);
+
+  return console.log(`${randomOne} ${randomTwo} ${randomThree} ${randomFour}`);
 };
 
-const generateMesssage = (objectWithWords) => {
-  const randomOne = Math.floor(Math.random() * objectWithWords.subjectWord.length);
-  const randomTwo = Math.floor(Math.random() * objectWithWords.centerWord.length);
-  const randomThree = Math.floor(Math.random() * objectWithWords.subjectWord.length);
-
-  return console.log(`${objectWithWords.subjectWord[randomOne]} ${objectWithWords.centerWord[randomTwo]} ${objectWithWords.subjectWord[randomThree].toLowerCase()}`);
-};
-
-generateMesssage(Messages);
+generateMesssage();
